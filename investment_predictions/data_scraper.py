a9cfa859658df21e88f215781ef36b47061a63dd
unknown <oldwhpsian@gmail.com>
2023-03-14 15:12:27 +0000

    Finished the DataScraper class and the associated unit tests

--- investment_predictions/data_scraper.py
+++ investment_predictions/data_scraper.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from pandas_datareader import data as pdr
 import pandas as pd
 import numpy as np
 from typing import List, Tuple, Dict, Union
@@ -6,52 +7,163 @@ import requests
 from requests import Response, Request
 import json
 from pathlib import Path
+import datetime as dt
 
+yf.pdr_override()
 
-key_path = Path().home() / "desktop" / "FinancialModellingPrep_API.txt"
-with open(key_path) as file:
-    api_key = file.read()
 
 class DataScraper:
-    def __init__(self, ticker: str, period: str='quarter'):
+    """
+    This class is used to scrape financial data from Financial Modeling Prep API and Yahoo Finance.
+
+    Args:
+        ticker (str): The ticker symbol for the stock to scrape data for.
+        api_key (str, optional): The API key to access Financial Modeling Prep API. Defaults to "".
+        period (str, optional): The period to retrieve data for. Can be 'annual' or 'quarter'. Defaults to 'quarter'.
+
+    Attributes:
+        ticker (str): The ticker symbol for the stock being scraped.
+        period (str): The period to retrieve data for.
+        api_key (str): The API key used to access Financial Modeling Prep API.
+        fmp_api_requests (list): The list of available data types to retrieve from Financial Modeling Prep API.
+        data_dictionary (dict): The dictionary of all data retrieved from Financial Modeling Prep API and Yahoo Finance.
+
+    Raises:
+        AssertionError: Raised if ticker, period, or api_key are not valid.
+
+    """
+    def __init__(self, ticker: str, api_key: str = "", period: str = "quarter"):
         self.ticker = ticker.upper()
         self.period = period.lower().strip()
-        self.fmp_api_requests = ['ratios', 'metrics', 'info', 'is']
-        # TODO: assert period is in [annual, quarter]
+        self.api_key = str(api_key)
+        self.fmp_api_requests = ["info", "ratios", "metrics", "is"]
+        self.assert_valid_user_inputs()
+        self.data_dictionary = self.fetch_all_data()
 
-    @staticmethod
-    def multi_line_string_stripper(string: str) -> str:
-        # TODO: strip and replace all whitespace and newlines with ''
-        string_ = string.replace(' ', '').replace('\t', '')
-        string_ = string_.replace('\n', '').replace
-        pass
+    def assert_valid_user_inputs(self):
+        """
+        Asserts that the inputs for the DataScraper class are valid.
 
-    def get_fmp_api_url(self, data_type: str='') -> str:
+        Raises:
+            AssertionError: Raised if ticker, period, or api_key are not valid.
+
+        """
+        assert self.ticker.isupper()
+        assert self.period.islower()
+        assert self.period in ["annual", "quarter"]
+        assert self.api_key
+
+    def get_fmp_api_url(self, data_type: str = "") -> str:
+        """
+        Returns the URL for a specific data type from Financial Modeling Prep API.
+
+        Args:
+            data_type (str, optional): The type of data to retrieve from Financial Modeling Prep API.
+
+        Returns:
+            str: The URL to retrieve the specified data type from Financial Modeling Prep API.
+
+        Raises:
+            AssertionError: Raised if the specified data_type is not valid.
+
+        """
         assert data_type in self.fmp_api_requests, f"<{data_type}> invalid"
-        if data_type == 'ratios':
-            template = "https://financialmodelingprep.com/api/"\
-                        "v3/ratios/{}?period={}&limit=400&apikey={}"
-            return template.format(self.ticker, self.period, api_key)
-        elif data_type == 'metrics':
-            template = "https://financialmodelingprep.com/api/v3/"\
-                            "key-metrics/{}?period={}&limit=400&apikey={}"
-            
-            return template.format(self.ticker, self.period, api_key)
-        elif data_type == 'info':
-            template = "https://financialmodelingprep.com/api/v3/"\
-                        "profile/{}?apikey={}"
-            return template.format(self.ticker, api_key)
-        else:
-            template = "https://financialmodelingprep.com/api/v3/"\
-                        "income-statement/{}?period={}&limit=400&apikey={}"
-            return template.format(self.ticker, self.period, api_key)
-    
+        if data_type == "ratios":
+            template = (
+                "https://financialmodelingprep.com/api/"
+                "v3/ratios/{}?period={}&limit=400&apikey={}"
+            )
+            return template.format(self.ticker, self.period, self.api_key)
+        if data_type == "metrics":
+            template = (
+                "https://financialmodelingprep.com/api/v3/"
+                "key-metrics/{}?period={}&limit=400&apikey={}"
+            )
+
+            return template.format(self.ticker, self.period, self.api_key)
+        if data_type == "info":
+            template = (
+                "https://financialmodelingprep.com/api/v3/" "profile/{}?apikey={}"
+            )
+            return template.format(self.ticker, self.api_key)
+        if data_type == "is":
+            template = (
+                "https://financialmodelingprep.com/api/v3/"
+                "income-statement/{}?period={}&limit=400&apikey={}"
+            )
+            return template.format(self.ticker, self.period, self.api_key)
+
     @staticmethod
     def make_fmp_api_requests(url: str) -> Response:
+        """
+        Makes an HTTP request to Financial Modeling Prep API and returns the response.
+
+        Args:
+            url (str): The URL to send the request to.
+
+        Returns:
+            Response: The response from the API request.
+
+        Raises:
+            AssertionError: Raised if the API request was unsuccessful.
+
+        """
         fmp_response = requests.get(url)
-        assert fmp_response.status_code == 200, 'Request unsuccessful'
+        assert fmp_response.status_code == 200, "Request unsuccessful"
         return fmp_response
 
     @staticmethod
-    def convert_raw_data_to_json(response: Response) -> json:
-        return response.json()
+    def convert_raw_data_to_json(response: Response) -> Dict:
+        """
+        Converts the response from an API request to a JSON object.
+
+        Args:
+            response (Response): The response object from the API request.
+
+        Returns:
+            dict: A dictionary representing the JSON object returned by the API request.
+
+        Raises:
+            AssertionError: Raised if the API request was successful but the response was empty.
+
+        """
+        json_data = response.json()
+        assert len(json_data) > 0, "API request successful but empty"
+        return json_data
+
+    def fetch_stock_price_data(self) -> pd.DataFrame:
+        """
+        Fetches stock price data from Yahoo Finance.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the stock price data.
+
+        Raises:
+            AssertionError: Raised if there is insufficient stock price data.
+
+        """
+        start = dt.date(1970, 1, 1)
+        stock_data = pdr.get_data_yahoo(self.ticker, start=start, interval="1d")
+        assert len(stock_data) > 85, "Insufficient stock price data"
+        return stock_data
+
+    def fetch_all_data(self) -> Dict[str, Dict]:
+        """
+        Fetches all financial data from Financial Modeling Prep API and Yahoo Finance.
+
+        Returns:
+            dict: A dictionary containing all the financial data retrieved.
+
+        Raises:
+            AssertionError: Raised if there is insufficient stock price data or if the API request is unsuccessful.
+
+        """
+        data_dictionary = {}
+        for string in self.fmp_api_requests:
+            url = self.get_fmp_api_url(string)
+            response = self.make_fmp_api_requests(url)
+            data = self.convert_raw_data_to_json(response)
+            data_dictionary[string] = data
+        stock_price_data = self.fetch_stock_price_data()
+        data_dictionary["price"] = [stock_price_data]
+        return data_dictionary
--- tests/test_metric_scraper.py
+++ tests/test_metric_scraper.py
@@ -1,73 +1,128 @@
+import sys
 from investment_predictions import DataScraper
 import unittest
 from unittest.mock import Mock, patch
 import itertools
 from pathlib import Path
 import requests
+import pandas as pd
 
-import sys
-sys.path.append('..')
+
+
+sys.path.append("..")
 
 key_path = Path().home() / "desktop" / "FinancialModellingPrep_API.txt"
 with open(key_path) as file:
     api_key = file.read()
 
 
-
 def instance_generator():
-    tickers = ['AAPL', 'NVDA', 'MSFT', 'JXN']
-    periods = ['annual', 'quarter']
+    """
+    Generator function that yields instances of the DataScraper class for various tickers and periods.
+
+    Yields:
+        DataScraper: An instance of the DataScraper class.
+
+    """
+    tickers = ["AAPL", "NVDA", "MSFT", "JXN"]
+    periods = ["annual", "quarter"]
     for ticker, period in itertools.product(tickers, periods):
-        yield DataScraper(ticker, period)
-    
+        yield DataScraper(ticker, api_key, period)
 
 
 class TestMetricScraper(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        pass
+    """
+    A unittest test case for the DataScraper class.
 
-    def test_multi_line_string_stripper(self):
-        pass
+    """
 
+    def test_assert_valid_user_inputs(self):
+        """
+        Test for the assert_valid_user_inputs method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if the ticker is not uppercase, if the period is not lowercase and not equal to 'annual' or 'quarter',
+            or if the api_key is empty.
+        """
+        for instance in instance_generator():
+            instance.assert_valid_user_inputs()
+            for i in range(10):
+                if i%2 == 0:
+                    instance.ticker = 'lol'
+                elif i%3 == 0:
+                    instance.period = 'OI'
+                else:
+                    instance.api_key = ''
+                self.assertRaises(AssertionError, 
+                                  instance.assert_valid_user_inputs)
+                
     def test_get_fmp_api_url(self):
+        """
+        Test for the get_fmp_api_url method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if the result of get_fmp_api_url does not match the expected URL.
+
+        """
         for instance in instance_generator():
             ticker = instance.ticker
             period = instance.period
             for string in instance.fmp_api_requests:
-                if string == 'ratios':
-                    template = "https://financialmodelingprep.com/api/"\
-                                "v3/ratios/{}?period={}&limit=400&apikey={}"
+                if string == "ratios":
+                    template = (
+                        "https://financialmodelingprep.com/api/"
+                        "v3/ratios/{}?period={}&limit=400&apikey={}"
+                    )
                     expected = template.format(ticker, period, api_key)
-                elif string == 'metrics':
-                    template = "https://financialmodelingprep.com/api/v3/"\
-                                    "key-metrics/{}?period={}&limit=400&apikey={}"
-                    
-                    expected= template.format(ticker, period, api_key)
-                elif string == 'info':
-                    template = "https://financialmodelingprep.com/api/v3/"\
-                                "profile/{}?apikey={}"
+                elif string == "metrics":
+                    template = (
+                        "https://financialmodelingprep.com/api/v3/"
+                        "key-metrics/{}?period={}&limit=400&apikey={}"
+                    )
+
+                    expected = template.format(ticker, period, api_key)
+                elif string == "info":
+                    template = (
+                        "https://financialmodelingprep.com/api/v3/"
+                        "profile/{}?apikey={}"
+                    )
                     expected = template.format(ticker, api_key)
                 else:
-                    template = "https://financialmodelingprep.com/api/v3/"\
-                                "income-statement/{}?period={}&limit=400&apikey={}"
+                    template = (
+                        "https://financialmodelingprep.com/api/v3/"
+                        "income-statement/{}?period={}&limit=400&apikey={}"
+                    )
                     expected = template.format(ticker, period, api_key)
 
                 result = instance.get_fmp_api_url(string)
                 self.assertEqual(result, expected)
 
     def test_make_fmp_api_request(self):
+        """
+        Test for the make_fmp_api_request method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if the response status code is not 200.
+
+        """
         for instance in instance_generator():
             mock_response = Mock(spec=requests.models.Response)
             mock_response.status_code = 200
             mock_response.content = b'{"result": "success"}'
 
-            with patch('requests.get', return_value=mock_response):
+            with patch("requests.get", return_value=mock_response):
                 url = "https://example.com/api"
                 response = instance.make_fmp_api_requests(url)
                 self.assertEqual(response.status_code, 200)
 
     def test_convert_raw_data_to_json(self):
+        """
+        Test for the convert_raw_data_to_json method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if the result of convert_raw_data_to_json does not match the expected result.
+
+        """
         for instance in instance_generator():
             mock_response = Mock(spec=requests.models.Response)
             mock_response.content = b'{"result": "success"}'
@@ -75,3 +130,39 @@ class TestMetricScraper(unittest.TestCase):
             expected = {"result": "success"}
             result = instance.convert_raw_data_to_json(mock_response)
             self.assertEqual(result, expected)
+
+    def test_fetch_stock_price_data(self):
+        """
+        Test for the fetch_stock_price_data method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if there is insufficient stock price data, if the columns of the DataFrame do not match the expected columns,
+            or if the returned object is not a pandas DataFrame.
+
+        """
+        for instance in instance_generator():
+            data = instance.fetch_stock_price_data()
+            self.assertGreater(len(data), 85)
+            cols = data.columns
+            expected_cols = pd.Index(
+                ["Open", "High", "Low", "Close", "Adj Close", "Volume"], dtype="object"
+            )
+            self.assertEqual(cols.equals(expected_cols), True)
+            self.assertIsInstance(data, pd.DataFrame)
+
+    def test_fetch_all_data(self):
+        """
+        Test for the fetch_all_data method of the DataScraper class.
+
+        Raises:
+            AssertionError: Raised if the length of the returned dictionary is not 5,
+            if a key in the returned dictionary is not a string, or
+            if the value of a key in the returned dictionary is not a list.
+
+        """
+        for instance in instance_generator():
+            data = instance.data_dictionary
+            self.assertEqual(len(data), 5)
+            for key in data.keys():
+                self.assertIsInstance(key, str)
+                self.assertIsInstance(data[key], list)
