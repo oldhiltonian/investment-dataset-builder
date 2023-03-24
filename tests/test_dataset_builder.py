add3e0da1fdd7c42d01e63bf9655564046fa2fa8
unknown <oldwhpsian@gmail.com>
2023-03-24 21:54:34 +0000

    Built the test_make_stock_ticker_api_request() unittest

--- tests/test_dataset_builder.py
+++ tests/test_dataset_builder.py
@@ -18,6 +18,8 @@ feature_path = Path.cwd() / "investment_predictions" / "features.json"
 with open(feature_path, "r") as f:
     features = json.load(f)
 
+def generate_class_instance():
+    return DatasetBuilder(['New York Stock Exchange'])
 
 class TestDatasetBuilder(unittest.TestCase):
 
@@ -28,8 +30,18 @@ class TestDatasetBuilder(unittest.TestCase):
         self.assertEqual(result, expected)
         self.assertNotEqual(expected, 'test')
 
-    def test_response_to_json(self):
-        pass
+    def test_make_stock_ticker_api_request(self):
+        url = "fakeurl.com"
+        response = Mock()
+        response.status_code = 200
+        instance = generate_class_instance()
+        with patch("requests.get", return_value=response) as mock_method:
+            result = instance.make_stock_ticker_api_request(url)
+            self.assertEqual(result, response)
+            mock_method.assert_called_once_with(url)
+            response.status_code = 100
+            with self.assertRaises(AssertionError):
+                instance.make_stock_ticker_api_request(url)
 
     def test_fetch_raw_stock_ticker_data(self):
         pass
@@ -37,11 +49,10 @@ class TestDatasetBuilder(unittest.TestCase):
     def test_set_exchanges(self):
         pass
 
-    def test_check_valid_security(self):
-        pass
-
     def test_build_dataset(self):
         pass
 
-    def test_validate_Data_is_float64(self):
+    def test_data_validation(self):
         pass
+
+    
